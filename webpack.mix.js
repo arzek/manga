17a6e3ee1d8225955aac0030608318654d4bde3f
gotoredo <gotoredo@gmail.com>
2018-01-14 19:14:53 +0200

    WAR BOOTSRAP

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('resources/assets/js/app.js', 'public/assets/js');
+
+mix.stylus('resources/assets/stylus/app.styl', 'public/assets/css');
+
+mix.styles([
+  'resources/assets/components/css/uikit.css',
+  'public/assets/css/app.css'
+], 'public/css/all.css');
